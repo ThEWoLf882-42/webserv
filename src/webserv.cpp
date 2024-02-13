32f607928e7d048bd85404be6d6b48aa17023ef3
ThEWoLf882-42 <agimi@student.1337.ma>
2024-02-13 11:21:45 +0100

    main

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 11:44:51 by agimi             #+#    #+#             */
-/*   Updated: 2024/02/13 10:14:55 by agimi            ###   ########.fr       */
+/*   Updated: 2024/02/13 11:19:05 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 int main(int ac, char **av)
 {
 	if (2 == ac)
-		wbs::Confile c(av[1]);
+		wbs::Server s(av[1]);
 	else
-		wbs::Confile c;
-	wbs::Server s(av[1]);
-	wbs::Server s;
+		wbs::Server s;
 }
