ce56b393ca643cfe2a01c6274b5724043e667504
ThEWoLf882-42 <agimi@student.1337.ma>
2024-02-21 15:19:59 +0100

    laaaaa

--- src/CGI/CGI.cpp
+++ src/CGI/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 11:00:59 by mel-moun          #+#    #+#             */
-/*   Updated: 2024/02/20 17:50:02 by agimi            ###   ########.fr       */
+/*   Updated: 2024/02/21 15:19:50 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ wbs::CGI &wbs::CGI::operator=(const CGI &ob)
 	if (this != &ob)
 	{
 		std::cout << "choofoonniii " << std::endl;
+		std::cout << "laaaaa " << std::endl;
 	}
 	return (*this);
 }
